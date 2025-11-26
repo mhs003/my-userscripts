c7802bd90450b4f4225ed961f8d49e0fe1e3878c
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2025-11-26 16:31:17 +0600

    Add phpMyAdmin auto login user script

---
+++ phpmyadmin-auto-login.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         phpMyAdmin auto login
+// @namespace    http://tampermonkey.net/
+// @version      2025-11-26
+// @description  try to take over the world!
+// @author       You
+// @match        http://localhost:8089/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function SC(scl) {
+        return document.querySelector(scl);
+    }
+
+    let count = 0;
+    let loginInterval = null;
+
+    loginInterval = setInterval(() => {
+        if(SC('[name="pma_username"]')) {
+            SC('[name="pma_username"]').value = 'root';
+        }
+
+        if(SC('[name="pma_password"]')) {
+            SC('[name="pma_password"]').value = '1234';
+        }
+
+        if(SC('[value="Log in"]')) {
+            SC('[value="Log in"]').click();
+            clearInterval(loginInterval);
+            return;
+        }
+        count++;
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(loginInterval);
+    }, 1000);
+
+})();
